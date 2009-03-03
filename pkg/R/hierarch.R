be3eb31cedcc9d5b53c39c11cc4e2b6a2c16e87b
natasak (Natasa Kejzar) <natasa.kejzar@mf.uni-lj.si>
2009-03-03 09:38:24 +0000

    Initial package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clustddist@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/hierarch.R
@@ -0,0 +1,318 @@
+# setwd("Users/natasa/projekti/R_package/paket/")
+#source("leaders.R")
+
+hierarch <- function (x, as.probs = TRUE, err.measure = "d4", penalty=1e6, nleaders=NULL){
+	#
+	# basic function, compute hierarchical clustering
+	# data: x (units with variable values), 
+	# error measure:err.measure
+	# penalty: number that substitutes x/0
+	# nleaders: number of leaders, that should be returned
+	# 
+    x <- data.matrix(x)
+    if (any(is.na(x)))
+        stop("Missing values not allowed.")
+    if (!is.numeric(x)) 
+        stop("x is not a numeric dataframe or matrix.")
+    x2 <- if (!as.probs) 
+          	x/apply(x, 1, sum)    # transform rows to probabilities
+          else x
+    n <- dim(x)[1]
+    if ((!is.null(nleaders)) && ((nleaders<1) || (nleaders>n)))
+    	stop("nleaders should be NULL or in [1,n].")
+    # compute clustering
+	clu <- hclustering(x2,err.measure,penalty,nleaders)
+	# compute order (to be able to plot dendrogram)
+	order <- compute.plot.order(clu$merge,dim(clu$merge)[1])
+    res <- list(merge=clu$merge,height=clu$height,order=order,
+    		labels=attr(x, "dimnames")[[1]],call=match.call(),method=err.measure) 
+    class(res) <- "hclust"
+    return(list(object=res,saved_leaders=clu$saved_leaders))
+}
+
+
+hclustering <- function(x,err.measure,penalty=1e6,nleaders){
+	n <- dim(x)[1]
+	merge <- matrix(nrow=n-1,ncol=2)
+	# temporary vector to save current cluster number - in which row it is written
+	# in order to construct "merge"
+	cluster_number <- seq(-1,-(n),-1)
+	height <- vector(mode="numeric",length=n-1)
+	leader <- as.matrix(x)
+	sum <- as.matrix(x)
+	harmonic <- harmonic.sum(x,penalty)
+	size <- rep(1,n)
+	# compute dissimilarity matrix
+	diss <- diss.matrix(leader,err.measure,size,sum,harmonic,penalty)
+	# (get lower triangle of dissimilarities)
+	saved_leaders <- NULL
+	
+	for (i in 1:(n-1)){
+		# compute minimal merging (diss)
+		min_dist <- min(diss[which(!is.na(diss))])
+		# update height
+		height[i] <- min_dist
+		index_min <- which(diss == min_dist,arr.ind = TRUE)
+		mini <- min(index_min[1,])
+		maxi <- max(index_min[1,])
+		# take first one that comes (*** according to columns!)
+		# UPDATE MERGE - with new cluster numbers!
+		merge[i,] <- c(cluster_number[mini],cluster_number[maxi])
+		cluster_number[mini] <- i
+		cluster_number <- cluster_number[-maxi] 
+		# END UPDATE MERGE
+		#	leader (compute new, add it on the first free place)
+		leader <- update.leader(mini,maxi,leader,err.measure,size,sum,harmonic,penalty)
+		if ((!is.null(nleaders)) && nleaders == i)
+			saved_leaders <- leader
+		# update size,sum,harmonic --- does not depend on err.measure
+		sum <- update.matrix(mini,maxi,sum)
+		size <- update.vector(mini,maxi,size)
+		harmonic <- update.matrix(mini,maxi,harmonic)
+		#	diss (compute new diss for new cluster)
+		diss <- update.diss(index_min[1,],diss,leader,err.measure,size,sum,harmonic,penalty)
+	}
+	rez <- list(merge=merge,height=height,saved_leaders=saved_leaders)
+    return(rez)
+}
+
+compute.plot.order <- function(merge,n){
+	#
+	# compute order of units in order to plot them
+	# in dendrogram (not having crossings on branches)
+	#
+	temp <- NULL
+	#print(merge[n,])	
+	if (min(merge[n,]) < 0){
+		temp <- abs(min(merge[n,]))
+		if (max(merge[n,]) < 0)
+			temp <- c(temp,abs(max(merge[n,])))
+		else
+			temp <- c(temp,compute.plot.order(merge,max(merge[n,])))
+		#print(temp)
+		return(temp)
+	}
+	else{
+		temp <- c(temp,compute.plot.order(merge,min(merge[n,])))
+		temp <- c(temp,compute.plot.order(merge,max(merge[n,])))
+		#print(temp)
+		return(temp)
+	}
+}
+
+update.vector <- function(mini,maxi,vec){
+	#
+	# subroutine to help merging 2 components (vectors) 
+	# (sum them and delete the latest)
+	#
+	vec[mini] <- vec[mini] + vec[maxi]
+	vec <- vec[-maxi]
+	return(vec)
+}
+
+update.matrix <- function(mini,maxi,mat){
+	#
+	# subroutine to help merging 2 components (row vectors inside matrix) 
+	# (sum them and delete the latest)
+	#
+	mat[mini,] <- mat[mini,] + mat[maxi,]
+	mat <- mat[-maxi,]
+	return(mat)
+}
+
+update.leader <- function(mini,maxi,leader,err.measure,size,sum,harmonic,penalty){
+	# compute leader when merging mini and maxi cluster
+	new_leader <- compute.hleader(mini,maxi,leader,err.measure,size,sum,harmonic,penalty)
+	# put it in the right position inside all leaders
+	dimension <- dim(leader)[1] - 1
+	leader <- leader[-maxi,]
+	if (dimension > 1)
+		leader[mini,] <- new_leader
+	else
+		leader <- t(as.matrix(new_leader))
+	return(leader)
+}
+
+update.diss <- function(index_min,diss,leader,err.measure,size,sum,harmonic,penalty,...){
+	#
+	# update according to merging i and j cluster (index_min)
+	# ... parameters: optional (depends upon err.measure)
+	# i < j: compute new D[i, ...], move all D[k, ...] one up (k > j)
+	# 		 delete D[... ,j]
+	# 
+	# diminish diss for 1 row and column (maxi)
+	mini <- min(index_min)
+	maxi <- max(index_min)
+	diss <- diss[-maxi,-maxi] 
+	if (mini != 1){
+		for (j in 1:(mini-1)){
+			i <- mini
+			d <- compute.D(i,j,leader,err.measure,size,sum,harmonic,penalty,...)
+			diss[i,j] <- d
+		}
+	}
+	if (!is.vector(diss) && (mini+1) < dim(diss)[1]){
+		for (i in (mini+1):dim(diss)[1]){
+			j <- mini
+			d <- compute.D(i,j,leader,err.measure,size,sum,harmonic,penalty,...)
+			diss[i,j] <- d
+		}
+	}
+	return(diss)
+}
+
+diss.matrix <- function(leader,err.measure,size,sum,harmonic,penalty,...){
+	# size (vector) - from 1s only, sum, harmonic (matrices)
+	n <- dim(leader)[1]
+	diss <- matrix(nrow=n,ncol=n)
+	for (i in 2:n){
+		for (j in 1:(i-1)){
+			d <- compute.D(i,j,leader,err.measure,size,sum,harmonic,penalty,...)
+			diss[i,j] <- d
+		}	
+	}
+	#print(diss)
+	return(diss)
+}
+
+compute.hleader <- function(i,j,leader,err.measure,size,sum,harmonic,penalty,...){
+	#
+	# subroutine that helps computing the one leader according to 
+	# err.measure and i and j clusters
+	#	
+	if (err.measure == "d1")
+		temp_leader <- d1.hleader(size[i],size[j],leader[i,],leader[j,])
+	if (err.measure == "d3")
+		temp_leader <- d3.hleader(leader[i,],leader[j,],sum[i,],sum[j,])
+	if (err.measure == "d4")
+		temp_leader <- d4.hleader(size[i],size[j],leader[i,],leader[j,])
+	if (err.measure == "d5")
+		temp_leader <- d5.hleader(leader[i,],leader[j,],harmonic[i,],harmonic[j,],penalty)
+	if (err.measure == "d6")
+		temp_leader <- d6.hleader(size[i],size[j],harmonic[i,],harmonic[j,])
+	if (err.measure == "d7")
+		temp_leader <- d7.hleader(leader[i,],leader[j,],sum[i,],sum[j,])
+	return(temp_leader)
+}
+
+compute.D <- function(i,j,leader,err.measure,size,sum,harmonic,penalty,...){
+	#
+	# subroutine that helps computing dissimilarity of i, j clusters
+	# according to err.measure
+	#	
+	if (err.measure == "d1"){
+		error <- eval(call(err.measure,leader[i,],t(as.matrix(leader[j,]))))
+		error <- error$error
+		d <- d1.D(size[i],size[j],error)
+	}else{
+	    # computation depends on error measure
+	    # compute leader (before computing d[i,j])
+	    temp_leader <- compute.hleader(i,j,leader,err.measure,size,sum,harmonic,penalty,...)
+		error_u <- eval(call(err.measure,leader[i,],t(as.matrix(temp_leader))))
+		error_u <- error_u$error
+		error_v <- eval(call(err.measure,leader[j,],t(as.matrix(temp_leader))))
+		error_v <- error_v$error
+ 		if (err.measure == "d3")
+			d <- d3.D(error_u,error_v,leader[i,],leader[j,],sum[i,],sum[j,],penalty)
+		if (err.measure == "d4")
+			d <- d4.D(size[i],size[j],error_u,error_v)
+		if (err.measure == "d5")
+			d <- d5.D(error_u,error_v,leader[i,],leader[j,],harmonic[i,],harmonic[j,])
+		if (err.measure == "d6")
+			d <- d6.D(size[i],size[j],error_u,error_v)
+		if (err.measure == "d7")
+			d <- d7.D(error_u,error_v,leader[i,],leader[j,],sum[i,],sum[j,],penalty)
+	}
+	return(d)
+}
+
+# new LEADER when two clusters merged
+
+d1.hleader <- function(size1,size2,leader1,leader2,...){
+	leader <- (size1*leader1 + size2*leader2)/(size1+size2)
+	return(leader)
+}
+
+d3.hleader <- function(leader1,leader2,sum1,sum2,...){
+	if (sum(sum1+sum2) == 0){
+		leader <- rep(0,length(leader1))	
+	}else{
+		leader <- (sum1*leader1 + sum2*leader2)/(sum1+sum2)
+		leader[which(is.nan(leader))] <- 0
+	}
+	return(leader)
+}
+
+d4.hleader <- function(size1,size2,leader1,leader2,...){
+	leader <- sqrt((size1*(leader1)^2 + size2*(leader2)^2)/(size1+size2))
+	return(leader)
+}
+
+d5.hleader <- function(leader1,leader2,harmonic1,harmonic2,penalty=1e6,...){
+	temp1 <- harmonic1/leader1
+	temp1[which(is.infinite(temp1))] <- penalty
+	temp2 <- harmonic2/leader2
+	temp2[which(is.infinite(temp2))] <- penalty
+	leader <- (harmonic1+harmonic2)/(temp1 + temp2)
+	return(leader)
+}
+
+d6.hleader <- function(size1,size2,harmonic1,harmonic2,...){
+	leader <- (size1+size2)/(harmonic1+harmonic2)
+	return(leader)
+}
+
+d7.hleader <- function(leader1,leader2,sum1,sum2,...){
+	if (sum(sum1+sum2) == 0){
+		leader <- rep(0,length(leader1))
+	}else{
+		leader <- sqrt((sum1+sum2)/(sum1/(leader1)^2+sum2/(leader2)^2))
+		leader[which(is.nan(leader))] <- 0
+	}
+	return(leader)
+}
+
+# DISSIMILARITY among two clusters
+
+d1.D <- function(size1,size2,error,...){
+	return(size1*size2/(size1+size2)*error)
+}
+
+d3.D <- function(error1,error2,leader1,leader2,sum1,sum2,penalty=1e6,...){
+	temp1 <- sum1/leader1*error1
+	temp1[which(is.infinite(temp1))] <- penalty
+	temp1[which(is.nan(temp1))] <- 0
+	temp2 <- sum2/leader2*error2
+	temp2[which(is.infinite(temp2))] <- penalty
+	temp2[which(is.nan(temp2))] <- 0
+	return(sum(temp1 + temp2))
+}
+
+d4.D <- function(size1,size2,error1,error2,...){
+	return(size1*error1 + size2*error2)
+}
+
+d5.D <- function(error1,error2,leader1,leader2,harmonic1,harmonic2,...){
+	return(sum(harmonic1*leader1*error1 + harmonic2*leader2*error2))
+}
+
+d6.D <- function(size1,size2,error1,error2,...){
+	return(d4.D(size1,size2,error1,error2,...))
+}
+
+d7.D <- function(error1,error2,leader1,leader2,sum1,sum2,penalty=1e6,...){
+	return(d3.D(error1,error2,leader1,leader2,sum1,sum2,penalty,...))
+}
+
+harmonic.sum <- function(x,penalty=1e6){
+	res <- 1/x
+	res[which(is.infinite(res))] <- penalty
+	return(res)	
+}
+
+
+# GLAVNI PROGRAM - TESTIRANJE
+#x <- read.table("test_short.txt",sep=" ",row.names = 1)
+#x <- as.matrix(x)
+#y <- hierarch(x,err.measure="d4")
+#plot(y$object)
---
+++ pkg/R/leaders.R
@@ -0,0 +1,396 @@
+# setwd("Users/natasa/projekti/R_package/paket/")
+
+leaders <- function (x, centers, err.measure = "d4", as.probs = FALSE,
+					iter.max = 10, stabil=1e-6, penalty=1e6){
+	#
+	# basic function, compute clusters
+	# data: x, number of clusters (or centers already): centers, error measure: err.measure
+	# if x is probabilities: as.probs, 
+	# number of iterations for method: iter.max
+	# (only considered if centers means "number of centers)
+	#
+    x <- data.matrix(x)
+    if (any(is.na(x)))
+        stop("Missing values not allowed.")
+    if (!is.numeric(x)) 
+        stop("x is not a numeric dataframe or matrix.")
+    x2 <- if (!as.probs) 
+              x/apply(x, 1, sum)    # transform rows to probabilities
+          else x
+
+    if (iter.max < 1) 
+        stop("'iter.max' must be positive.")
+    if (missing(centers)) 
+        stop("'centers' must be a number or a matrix.")
+    if (length(centers) == 1) {
+        k <- centers
+        iter <- TRUE
+        if (nrow(unique(x2)) < k || k < 1)
+            stop("Number of clusters must be in {1,2, .., n}.")
+    }
+    else {
+    	iter <- FALSE
+    	iter.max <- 1
+    	centers2 <- as.matrix(centers)
+    	centers <- if (!as.probs)
+    		centers2/apply(centers2,1,sum)
+    			else centers2
+        if (any(duplicated(centers))) 
+            stop("Initial centers are not distinct.")
+        if (!(ncol(x2) == ncol(centers))) 
+            stop("Must have same number of columns in 'x' and 'centers'.")
+
+        k <- nrow(centers)
+        if (nrow(unique(x2)) < k) 
+            stop("More cluster centers than data units.")
+    }
+	
+    res <- NULL
+	for (i in 1:iter.max){
+		if (iter){
+		    # compute initial "leaders"
+        	ux2 <- unique(x2)
+        	ur <- nrow(ux2)
+        	centers <- ux2[sample(1:ur, k), , drop = FALSE]
+		}
+        # compute clustering
+        curr_res <- leaders.algorithm(x2, centers, err.measure=err.measure,
+        		stabil=stabil,penalty=penalty)
+        if (is.null(res) || res$error > curr_res$error)
+        	res <- curr_res
+	}
+
+    out <- list(clustering=res$clu_vector,error=res$error,centers=res$leaders)
+    #class(out) <- c("leaders","partition")
+    return(out)
+}
+
+leaders.algorithm <- function(x, centers, err.measure="d4",stabil=1e-6,
+					penalty=1e6){
+	#
+	# algorithm to get k=dim(centers)[1] clusters
+	# error measure between unit and center (leader): err.measure
+	# when leaders stabilize: stabil
+	#
+	if (is.null(rownames(x))){
+		temp <- 1:dim(x)[1]
+		rownames(x) <- as.character(temp)	# names for each unit
+	}
+	centers <- sort.matrix.rows(centers)
+	k <- dim(centers)[1]
+	# compute initial clusters (according to err.measure)
+	curr_result <- compute.clusters(x,centers,err.measure,penalty)
+	iter = 0
+	stabilize = FALSE
+	while (!stabilize){
+		# compute new leaders
+		leaders <- compute.leaders(x,k,curr_result$clu_vector,err.measure)
+		leaders <- sort.matrix.rows(leaders)
+		# compute clustering function and check if leaders stabilized
+		if (max(centers - leaders) < stabil)
+			stabilize = TRUE
+		else{
+			# compute new clusters
+			curr_result <- compute.clusters(x,leaders,err.measure,penalty)
+			centers <- leaders
+			iter <- iter+1
+			print(iter)
+			print(curr_result$error)
+		}
+	}
+	return(list(clu_vector=curr_result$clu_vector,error=curr_result$error,leaders=leaders))
+}
+
+### SUBROUTINES TO BREAK THE CODE ###
+
+compute.clusters <- function(x, centers, err.measure, penalty=1e6){
+	#
+	# compute clustering vector: names: units, values: cluster numbers (1:k)
+	# k: number of centers
+	# calls "err.measure" function for each unit
+	#
+	clu_vector <- vector(mode="numeric",length=dim(x)[1])
+	names(clu_vector) <- rownames(x)
+	error <- 0
+	for (i in 1:dim(x)[1]){
+		temp_e <- eval(call(err.measure,x[i,],centers,penalty))
+		clu_vector[i] <- temp_e$clu
+		error <- error + temp_e$error
+	}
+	return(list(clu_vector=clu_vector,error=error))
+}
+
+compute.leaders <- function(x, k, curr_clusters,err.measure){
+	#
+	# data: x, number of clusters: k, curr_clusters: units with current clusters
+	# calls "error.measure.leader" function for each cluster
+	#
+	leaders <- matrix(nrow=k,ncol=dim(x)[2])
+	for (i in 1:k){
+		cluster_units <- x[which(curr_clusters == i),]
+		if (!is.matrix(cluster_units))	# only one row
+			cluster_units <- t(as.matrix(cluster_units))
+		leaders[i,] <- eval(call(paste(err.measure,"cleader",sep="."),cluster_units))
+	}
+	return(leaders)	
+}
+
+sort.matrix.rows <- function(m){
+	
+	df <- as.data.frame(m)
+	df_sorted <- df[do.call(order,df),]
+	m_sorted <- as.matrix(df_sorted)
+	dimnames(m_sorted) <- dimnames(m)
+	return(m_sorted)	
+}
+
+### ERROR MEASURES ###
+
+d1 <- function(unit, centers,penalty=1e6){
+	#
+	# compute error term for one unit
+	# return number of center and error there
+	#
+	possible_d <- vector(mode="numeric",length=dim(centers)[1])
+	for (i in 1:dim(centers)[1]){
+		temp <- (unit-centers[i,])^2
+		possible_d[i] <- sum(temp) 
+	}
+	clu <- which.min(possible_d)
+	error <- possible_d[clu]
+	return(list(error=error,clu=clu))
+}
+
+d1.cleader <- function(cluster_units){
+	#
+	# compute new leader for a cluster of units
+	# return leader
+	#
+	leader <- apply(cluster_units,2,mean)
+	return(leader)
+}
+
+d2 <- function(unit, centers,penalty=1e6){
+	#
+	# compute error term for one unit
+	# return number of center and error there
+	#
+	possible_d <- vector(mode="numeric",length=dim(centers)[1])
+	for (i in 1:dim(centers)[1]){
+		temp <- abs(unit-centers[i,])
+		possible_d[i] <- sum(temp) 
+	}
+	clu <- which.min(possible_d)
+	error <- possible_d[clu]
+	return(list(error=error,clu=clu))
+}
+
+d2.cleader <- function(cluster_units){
+	#
+	# compute new leader for a cluster of units
+	# return leader
+	#
+	leader <- apply(cluster_units,2,median)
+	return(leader)
+}
+
+d3 <- function(unit, centers,penalty=1e6){
+	#
+	# compute error term for one unit
+	# return number of center and error there
+	#
+	possible_d <- vector(mode="numeric",length=dim(centers)[1])
+	for (i in 1:dim(centers)[1]){
+		temp <- ((unit-centers[i,])/centers[i,])^2
+		# solving problems of zero values (unit or centers component)
+		# both 0 -> NaN becomes 0
+		temp[which(is.nan(temp))] <- 0
+		# centers 0 -> turn to penalty (large number)
+		temp[which(is.infinite(temp))] <- penalty
+		possible_d[i] <- sum(temp) 
+	}
+	clu <- which.min(possible_d)
+	error <- possible_d[clu]
+	return(list(error=error,clu=clu))
+}
+
+d3.cleader <- function(cluster_units){
+	#
+	# compute new leader for a cluster of units
+	# return leader
+	#
+	leader <- apply(cluster_units^2,2,mean)/(apply(cluster_units,2,mean))
+	leader[which(is.nan(leader))] <- 0 # define 0/0 = 0
+	return(leader)
+}
+
+d4 <- function(unit, centers,penalty=1e6){
+	#
+	# compute error term for one unit
+	# return number of center and error there
+	#
+	possible_d <- vector(mode="numeric",length=dim(centers)[1])
+	for (i in 1:dim(centers)[1]){
+		temp <- (unit-centers[i,])^2/centers[i,]
+		# solving problems of zero values (unit or centers component)
+		# both 0 -> NaN becomes 0
+		temp[which(is.nan(temp))] <- 0
+		# centers 0 -> turn to penalty (large number)
+		temp[which(is.infinite(temp))] <- penalty
+		possible_d[i] <- sum(temp) 
+	}
+	clu <- which.min(possible_d)
+	error <- possible_d[clu]
+	return(list(error=error,clu=clu))
+}
+
+d4.cleader <- function(cluster_units){
+	#
+	# compute new leader for a cluster of units
+	# return leader
+	#
+	leader <- sqrt(apply(cluster_units^2,2,mean))
+	return(leader)
+}
+
+d5 <- function(unit, centers,penalty=1e6){
+	#
+	# compute error term for one unit
+	# return number of center and error there
+	#
+	possible_d <- vector(mode="numeric",length=dim(centers)[1])
+	for (i in 1:dim(centers)[1]){
+		temp <- ((unit-centers[i,])/unit)^2
+		# solving problems of zero values (unit or centers component)
+		# both 0 -> NaN becomes 0
+		temp[which(is.nan(temp))] <- 0
+		# centers 0 -> turn to penalty (large number)
+		temp[which(is.infinite(temp))] <- penalty
+		possible_d[i] <- sum(temp) 
+	}
+	clu <- which.min(possible_d)
+	error <- possible_d[clu]
+	return(list(error=error,clu=clu))
+}
+
+d5.cleader <- function(cluster_units){
+	#
+	# compute new leader for a cluster of units
+	# return leader
+	#
+	penalty <- 1e6
+	leader <- vector(mode="numeric",length=dim(cluster_units)[2])
+	x <- 1/cluster_units
+	for (i in 1:dim(cluster_units)[2]){
+		# compute every column (leader variable) separately
+		infinite <- which(is.infinite(x[,i]))
+		if (length(infinite) == dim(cluster_units)[1])
+			leader[i] <- 0	# all zeros
+		else{
+			x[infinite,i] <- penalty # zeros get penalty
+			x2 <- x[,i]^2 # KAKO UPORABITI PENALTY tukaj!?!
+			leader[i] <- sum(x[,i])/sum(x2)
+		}	
+	}
+	return(leader)
+}
+
+d6 <- function(unit, centers,penalty=1e6){
+	#
+	# compute error term for one unit
+	# return number of center and error there
+	#
+	possible_d <- vector(mode="numeric",length=dim(centers)[1])
+	for (i in 1:dim(centers)[1]){
+		temp <- (unit-centers[i,])^2/unit
+		# solving problems of zero values (unit or centers component)
+		# both 0 -> NaN becomes 0
+		temp[which(is.nan(temp))] <- 0
+		# centers 0 -> turn to penalty (large number)
+		temp[which(is.infinite(temp))] <- penalty
+		possible_d[i] <- sum(temp) 
+	}
+	clu <- which.min(possible_d)
+	error <- possible_d[clu]
+	return(list(error=error,clu=clu))
+}
+
+d6.cleader <- function(cluster_units){
+	#
+	# compute new leader for a cluster of units
+	# return leader
+	#
+	penalty <- 1e6
+	leader <- vector(mode="numeric",length=dim(cluster_units)[2])
+	x <- 1/cluster_units
+	for (i in 1:dim(cluster_units)[2]){
+		# compute every column (leader variable) separately
+		infinite <- which(is.infinite(x[,i]))
+		if (length(infinite) == dim(cluster_units)[1])
+			leader[i] <- 0	# all zeros
+		else{
+			x[infinite,i] <- penalty # zeros get penalty
+			leader[i] <- 1/mean(x[,i])
+		}	
+	}
+	return(leader)
+}
+
+d7 <- function(unit, centers,penalty=1e6){
+	#
+	# compute error term for one unit
+	# return number of center and error there
+	#
+	possible_d <- vector(mode="numeric",length=dim(centers)[1])
+	for (i in 1:dim(centers)[1]){
+		temp <- (unit-centers[i,])^2/(unit*centers[i,])
+		# solving problems of zero values (unit or centers component)
+		# both 0 -> NaN becomes 0
+		temp[which(is.nan(temp))] <- 0
+		# centers 0 -> turn to penalty (large number)
+		temp[which(is.infinite(temp))] <- penalty
+		possible_d[i] <- sum(temp) 
+	}
+	clu <- which.min(possible_d)
+	error <- possible_d[clu]
+	return(list(error=error,clu=clu))
+}
+
+d7.cleader <- function(cluster_units){
+	#
+	# compute new leader for a cluster of units
+	# return leader
+	#
+	penalty <- 1e6
+	leader <- vector(mode="numeric",length=dim(cluster_units)[2])
+	x <- 1/cluster_units
+	for (i in 1:dim(cluster_units)[2]){
+		# compute every column (leader variable) separately
+		infinite <- which(is.infinite(x[,i]))
+		if (length(infinite) == dim(cluster_units)[1])
+			leader[i] <- 0	# all zeros
+		else{
+			x[infinite,i] <- penalty # zeros get penalty
+			leader[i] <- sqrt(mean(cluster_units[,i])/mean(x[,i]))
+		}	
+	}
+	return(leader)
+}
+
+# GLAVNI PROGRAM - TESTIRANJE
+#x <- read.table("test.txt",sep=" ",row.names = 1)
+#centers <- x[1:3,]
+#centers <- 3
+#tt <- system.time(c <- leaders(x, centers=centers)) # izpisujejo se iteracije in trenutna napaka
+#print(tt)
+#print(c)
+
+#[1] 41 - iterations
+#[1] 2313.275
+#   user  system elapsed 
+# 31.432   0.299  31.808 
+
+#[1] 32 - iterations
+#[1] 2301.801
+#   user  system elapsed 
+# 24.638   0.217  24.873 
